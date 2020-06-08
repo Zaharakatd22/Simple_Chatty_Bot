0e5301fb2d31fe25b9374d5aff4907a20328657c
Zaharakatd <37711267+Zaharakatd22@users.noreply.github.com>
2020-06-08 14:50:53 +0400

    Add guess user age method

--- bot.py
+++ bot.py
@@ -1,5 +1,8 @@
 def main():
     class SimpleChattyBot:
+        user_name: str = ""
+        user_age: int = 0
+
         def __init__(self, bot_name: str, bot_birth_year: int):
             self.bot_name: str = bot_name
             self.bot_birth_year: int = bot_birth_year
@@ -15,7 +18,12 @@ def main():
                   f"I was create in {self.bot_birth_year}")
 
         def greeting_user(self, user_name: str):
-            print(f"What a great name you have, {user_name}!")
+            SimpleChattyBot.user_name = user_name
+            print(f"What a great name you have, {SimpleChattyBot.user_name}!")
+
+        def guess_age(self, r3, r5, r7):
+            SimpleChattyBot.user_age = (r3 * 70 + r5 * 21 + r7 * 15) % 105
+            print(f"Your age is {SimpleChattyBot.user_age}; that's a good time to start programming!")
 
     my_chatty_bot = SimpleChattyBot("Aid", 2020)
 
@@ -24,6 +32,13 @@ def main():
     name: str = input("Please, remind me your name. \n"
                       "> ")
     my_chatty_bot.greeting_user(name)
+
+    print("Let me guess your age.")
+    print("Enter remainders of dividing your age by 3, 5 and 7.")
+    remainder_3: int = int(input("> "))
+    remainder_5: int = int(input("> "))
+    remainder_7: int = int(input("> "))
+    my_chatty_bot.guess_age(remainder_3, remainder_5, remainder_7)
 
 
 if __name__ == "__main__":
